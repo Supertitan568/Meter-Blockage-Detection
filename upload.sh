0a9b06b2c36629bc36929eef19d139462b87ef42
Supertitan568 <laodenbach@gmail.com>
2025-03-13 11:38:13 -0500

    Added functionality with upload.sh

--- upload.sh
+++ upload.sh
@@ -11,30 +11,45 @@ upload (){
   make'
 }
 
-unzip(){
-  working_dir="~/coding/meter_blockage_detection/microwave/"
-  tar xzvf meter_blockage.tar.gz $working_dir
-  cd $working_dir/meter_blockage_detection/
-  make
+check_rasp_pi_connection(){
+   pi_mac_addr=2c:cf:67:4e:f4:fe
+   # ARP tables only seem to fill after running this
+   nmap -sn 192.168.1.0/24 > /dev/null  
+
+   lines=$(ip neighbor)
+   while IFS= read -r line; do 
+     ip_addr=$(echo $line | cut -d" " -f 1 )
+     mac_addr=$(echo $line | cut -d" " -f 5 )
+     if [[ $mac_addr == $pi_mac_addr ]]; then
+        echo "Found Raspberry Pi at $ip_addr"
+        break
+     fi
+   done <<< "$lines"
 }
 
-if [ -z ${1+x} ] | [ -z ${2+x} ]; then
-  echo "Error: This script needs to cli args"
-  echo "./upload.sh <action> <ip addr>"
-  exit
-fi
+if [ -z ${1+x} ]; then
+      echo "Error: This script needs one or two cli args"
+      echo "./upload.sh <action> <ip addr>"
+      exit
+fi 
 
 case $1 in 
   "upload")
+    if [ -z ${2+x} ]; then
+      echo "Error: This script needs two cli args"
+      echo "./upload.sh <action> <ip addr>"
+      exit
+    fi 
     echo "Uploading..."
     upload $2
     ;;
-  "unzip")
-    echo "Unziping and running..."
-    unzip 
+  "check_connection")
+    
+    echo "Checking Raspberry Pi Connection..."
+    check_rasp_pi_connection
     ;;
   *)
-    echo "Running everything..";
+    echo "Invalid Command";
     ;;
 esac
 
